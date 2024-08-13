5015d8ee4d1c7f14fccd9852bb1c844cf8bc1190
jv3ga <joel.v3ga@gmail.com>
2024-08-13 04:40:54 +0200

    fix: Oracle ORM backend compatibility #180 (#186)

--- django_q/monitor.py
+++ django_q/monitor.py
@@ -108,12 +108,12 @@ def save_task(task, broker: Broker):
             filters[Conf.SAVE_LIMIT_PER] = value
 
         with db.transaction.atomic(using=db.router.db_for_write(Success)):
-            last = Success.objects.filter(**filters).select_for_update().last()
+            list(Success.objects.filter(**filters).select_for_update())
             if (
                 task["success"]
                 and 0 < Conf.SAVE_LIMIT <= Success.objects.filter(**filters).count()
             ):
-                last.delete()
+                Success.objects.filter(**filters).last().delete()
 
         # check if this task has previous results
         try:
